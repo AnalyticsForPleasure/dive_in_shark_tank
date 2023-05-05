3a1b3e4c46bd1ff22cadb8a70a7564407504d0bf
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-05 11:32:17 +0300

    Need to deal with several problems in the data and chart

--- Entrepreneur_Gender_chart.py
+++ Entrepreneur_Gender_chart.py
@@ -6,28 +6,29 @@ import matplotlib.lines as mlines
 
 
 def creating_the_data_gender(df_2):
-    global genders, gender_counts, n, x
-    genders = np.unique(df_2['Entrepreneur Gender'])
-    gender_counts = np.zeros(len(genders))  # initialize the array
+
+    genders_df = np.unique(df_2['Entrepreneur Gender'])
+    gender_counts = np.zeros(len(genders_df))  # initialize the array
     # in order to fill the array we will use a loop
-    for n, gender in enumerate(genders):
+    for n, gender in enumerate(genders_df):
         gender_counts[n] = 100 * (df_2['Entrepreneur Gender'] == gender).sum() / len(df_2)
     print('*')
-    pos_gender_chart = np.arange(len(genders))
+    pos_gender_chart = np.arange(len(genders_df))
 
-    return pos_gender_chart, gender_counts
+    return pos_gender_chart, gender_counts , genders_df
 
 
-def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter):
+def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter , genders_df):
 
     fig, ax = plt.subplots(figsize=(12, 4))
     w = 0.6
     barlist = ax.bar(pos_gender, gender_counter, width=w, alpha=0.9)
-    ax.set_xticklabels(genders)
+    ax.set_xticklabels(genders_df)
+
     # Plot the metric
     ax.set_ylim([-2, 62])
     ax.set_xticks(pos_gender)
-    ax.set_yticks(np.arange(0, 81, 20))
+    ax.set_yticks(np.arange(0, 61, 20))
     ax.set_ylabel('Percent of entrepreneurs', fontsize=12)
     ax.tick_params(labelsize=12)
     # ax.set_xticklabels(genders, weight='heavy', fontsize=FS+2)
@@ -40,7 +41,7 @@ def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter):
     for b, c in zip(barlist, ['pink', 'skyblue', 'orange']):
         b.set_color(c)
     for n in np.arange(len(pos_gender_chart)):
-        ax.text(gender_counter[n], 2, "{g}".format(g=genders[n]),
+        ax.text(gender_counter[n], 2, "{g}".format(g=genders_df[n]),
                 fontsize=12, ha='center', color='w', weight='heavy')
     mid = ax.get_xlim()[0]
     # ax.set_title('Gender representation on Shark Tank', fontsize=FS, weight='heavy')
@@ -48,8 +49,7 @@ def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter):
     y = 64
     ax.text(xlim[0] + 0.02, y, 'Gender representation on', fontdict=params)
     ax.text(xlim[0] + 0.879, y, 'Shark Tank', fontdict=params, style='italic')
-    # ax.text(xlim[-1]-0.03, 56, '@ johnwmillr', fontdict=params, fontsize=FS-2, alpha=0.4, ha='right', rotation=-90)
-    ax.text(xlim[-1] - 0.03, y, '@ ShayCo', fontdict=params, fontsize=12, alpha=0.4, ha='right')
+    ax.text(xlim[-1] - 0.03, y, 'Analytics For Pleasure', fontdict=params, fontsize=12, alpha=0.4, ha='right')
     plt.show()
 
 
@@ -61,6 +61,6 @@ if __name__ == '__main__':
 
     #creating the Data (before adding a plot )
 
-    pos_gender_chart ,gender_counts = creating_the_data_gender(df_2)
-    horizontal_bar_plot_for_gender_enterpreneur(pos_gender_chart ,gender_counts )
+    pos_gender_chart ,gender_counts , genders_df = creating_the_data_gender(df_2)
+    horizontal_bar_plot_for_gender_enterpreneur(pos_gender_chart ,gender_counts , genders_df )
     print('*')
