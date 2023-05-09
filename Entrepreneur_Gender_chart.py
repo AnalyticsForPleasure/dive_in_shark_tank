3a9928b7b96b615ec6f8301f2dc316db46cccfd6
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-09 07:31:52 +0300

    counting_close_deal_by_gender - getting the percentage info

--- Entrepreneur_Gender_chart.py
+++ Entrepreneur_Gender_chart.py
@@ -3,6 +3,27 @@ import numpy as np
 import pandas as pd
 
 # **************************************************************************************************************
+# Function  name: counting_close_deal_by_gender
+# input:
+# return value:
+# ****************************************************************************************************************
+def counting_close_deal_by_gender(df_2):
+    all_the_deals_closed = df_2.loc[df_2['Deal'] == 'Yes', :]
+    res = all_the_deals_closed['Entrepreneur Gender'].value_counts()
+    res = res.reset_index(level=0)
+    res.rename(columns={res.columns[0]: 'Gender'}, inplace=True)
+    res.rename(columns={res.columns[1]: 'Counter close deals by gender'}, inplace=True)
+    print('*')
+
+    list_of_sharks_who_gave_pitch_by_gender = [535,221,139]
+    grouping_by_gender_who_got_funded = np.zeros(len(list_of_sharks_who_gave_pitch_by_gender))
+    index = 1
+    for index in list_of_sharks_who_gave_pitch_by_gender:
+        grouping_by_gender_who_got_funded[index] = res.loc[index,'Counter close deals by gender'] / list_of_sharks_who_gave_pitch_by_gender[index]
+        index += 1
+        print('*')
+    return 3
+# **************************************************************************************************************
 # Function  name: creating_the_data_gender
 # input:
 # return value:
@@ -17,7 +38,7 @@ def creating_the_data_gender(df_2):
     # in order to fill the array we will use a loop
     for n, gender in enumerate(genders_df_new):
         gender_counts[n] = 100 * (df_2['Entrepreneur Gender'] == gender).sum() / len(df_2)
-    print('*')
+        print('*')
     pos_gender_chart = np.arange(len(genders_df_new))
     print('*')
     return pos_gender_chart, gender_counts, genders_df_new
@@ -56,7 +77,7 @@ def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter, gend
     # TODO : need to add a counter anaimation for each bar, which will present the value of the bar
     # plt.title('Deaths per Country \n' + str(df1.index[iv].strftime('%y-%m-%d'))) # taken from  https://medium.com/towards-data-science/learn-how-to-create-animated-graphs-in-python-fce780421afe
 
-    for n in np.arange(len(pos_gender_chart)): # this line is correct
+    for n in np.arange(len(pos_gender_chart)):
         ax.text(x=pos_gender_chart[n], y=3.5, s="{g}".format(g=genders_df_new[n]), va='bottom', fontsize=12, # this line is correct
                 ha='center', color='w', weight='heavy', alpha=1)
 
@@ -72,17 +93,14 @@ def horizontal_bar_plot_for_gender_enterpreneur(pos_gender, gender_counter, gend
     y = 64
     ax.text(x=xlim[0] + 0.02, y=y, s='Gender representation on', fontdict=params)
     ax.text(x=xlim[0] + 0.879, y=y, s='Shark Tank', fontdict=params, style='italic')
-    ax.text(x=xlim[-1] - 0.03, y=y, s='AnalyticsForPleasure', fontdict=params, fontsize=12, alpha=0.4, ha='right')
+    ax.text(x=xlim[-1] - 0.03, y=y, s='AnalyticsForPleasure', fontdict=params, alpha=0.4, ha='right')
 
     plt.show()
 
 
 
-
-
 if __name__ == '__main__':
-    df_2 = pd.read_excel(r'C:/Users/Gil/PycharmProjects/dive_into_shark_tank/Data/shark_tank_data.xlsx',
-                         sheet_name='Sheet1')  #
+    df_2 = pd.read_excel(r'C:/Users/Gil/PycharmProjects/dive_into_shark_tank/Data/shark_tank_data.xlsx',sheet_name='Sheet1')  #
     df_2 = df_2.fillna(' ')
     print('*')
 
@@ -92,37 +110,5 @@ if __name__ == '__main__':
     #horizontal_bar_plot_for_gender_enterpreneur(pos_gender_chart, gender_counts, genders_df)
     #print('*')
 
-    #Creating a dictionary:
-    sharks = ['Corcoran', 'Greiner', 'Cuban', 'Herjavec', 'John', "O'Leary"]
-    shark_genders = dict(zip(sharks,['f','f','m','m','m','m']))
-
-    # for shark in sharks:
-    #     df_2['shark'].fillna(0, inplace=True)
+    counting_close_deal_by_gender(df_2)
     print('*')
-
-    # Ratios of men and women appearing on show
-    gender_ratios = {'m': 100*(df_2['Entrepreneur Gender'] =='Male').sum()/df_2.shape[0],
-                     'f': 100*(df_2['Entrepreneur Gender'] =='Female').sum()/df_2.shape[0]}
-
-    genders_df = np.unique(df_2['Entrepreneur Gender'])
-    index = [0]
-    remove_element = np.array([0])
-    genders_df_new = np.delete(genders_df, index)
-
-    gender_labels = genders_df_new[0:2]  #  gender_labels => ['Female' 'Male']
-    fig, axs = plt.subplots(ncols=len(sharks), sharey=True, figsize=(14,6))
-    idxs = np.linspace(0,1,num=9)
-    for ax,shark,idx in zip(axs,sharks,idxs):
-
-        percent_funded = []
-
-        for n,gender in enumerate(gender_labels):
-            num_gender = (df_2['Entrepreneur Gender']==gender).sum()
-            percent_funded.append(100*((df_2['Entrepreneur Gender']==gender) * df_2['# Sharks']).sum()/df_2['# Sharks'].sum())
-            # 100*((df.gender==gender).values * df[shark].values).sum()/df[shark].sum()-gender_ratios[gender[0].lower()])
-            print('*')
-    #
-    # # Plot the reference line
-    # ax.plot([ 0.6, 1.4], gender_ratios['m']*np.array([1, 1]), color='k', linestyle='--')
-    # ax.plot([-0.4, 0.4], gender_ratios['f']*np.array([1, 1]), color='k', linestyle='--')
-
