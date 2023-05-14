74eddfaece860196013bb4268a9fe62097b5a072
Shay Cohen <shaycohen2003@gmail.com>
2023-05-14 21:54:39 +0300

    Update royalty_equity_loans.py

--- royalty_equity_loans.py
+++ royalty_equity_loans.py
@@ -42,3 +42,30 @@ if __name__ == '__main__':
 
 
 #https://towardsdatascience.com/visualizing-intersections-and-overlaps-with-python-a6af49c597d9
+
+
+
+
+    # The total amount of agreements made by each shark:
+    Barbara_total_investment = panda_df.iloc[0,:].to_list()
+    Mark_total_investment = panda_df.iloc[1, :].to_list()
+    Lori_total_investment = panda_df.iloc[0, :].to_list()
+    Robert_total_investment = panda_df.iloc[0, :].to_list()
+    Daymond_total_investment = panda_df.iloc[0, :].to_list()
+    Kevin_total_investment = panda_df.iloc[0, :].to_list()
+
+    # The  amount of total agreements made:
+    Total_equity_investments_agreements_made_by_all_sharks = panda_df.iloc[:,0].to_list()
+    Total_convertible_note_agreements_made_by_all_sharks = panda_df.iloc[:,1].to_list()
+    Total_revenue_sharing_agreement_made_by_all_sharks = panda_df.iloc[:, 2].to_list()
+    Total_royalty_agreement_made_by_all_sharks = panda_df.iloc[:, 3].to_list()
+    Total_loan_agreement_made_by_all_sharks = panda_df.iloc[:, 4].to_list()
+
+
+    venn2([set(Barbara_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+           set_colors=('#3E64AF', '#3EAF5D'),
+           set_labels=('Total\ninvestments\n of Barbara',
+                       'Total\n Equity investment \n  agreement'),
+                       alpha=0.75)
+    venn2_circles([set(Barbara_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)], lw=0.7)
+    plt.show()
