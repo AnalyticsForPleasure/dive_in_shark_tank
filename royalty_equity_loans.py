12a2945a5e5d403a4daf8a3ba2b5ebde52f38a5c
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-15 07:56:12 +0300

    Venn diagram - need to correct the list of labels. 2 list of labels

--- royalty_equity_loans.py
+++ royalty_equity_loans.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from matplotlib_venn import venn2, venn2_circles
 import numpy as np
 import pandas as pd
 
@@ -42,3 +43,107 @@ if __name__ == '__main__':
 
 
 #https://towardsdatascience.com/visualizing-intersections-and-overlaps-with-python-a6af49c597d9
+
+    #sum of coloumns in a specific row of the dataframe
+    panda_df['sum_of_agreements_for_each_shark'] = panda_df.iloc[:, :5].sum(axis=1)
+
+    #sum of rows in the specific columns of the dataframe
+    sum_of_row = panda_df.iloc[:panda_df.shape[0], :].sum(axis=0)
+    panda_df.loc[panda_df.shape[0], 0:6] = sum_of_row
+
+    # rename the index of a specific row:
+    panda_df = panda_df.rename(index={6: 'sum of each agreement'})
+
+    print('*')
+
+    # The total amount of agreements made by each shark:
+    Barbara_total_investment = panda_df.iloc[0,:].to_list()
+    Mark_total_investment = panda_df.iloc[1, :].to_list()
+    Lori_total_investment = panda_df.iloc[2, :].to_list()
+    Robert_total_investment = panda_df.iloc[3, :].to_list()
+    Daymond_total_investment = panda_df.iloc[4, :].to_list()
+    Kevin_total_investment = panda_df.iloc[5, :].to_list()
+
+    # The  amount of total agreements made:
+    Total_equity_investments_agreements_made_by_all_sharks = panda_df.iloc[:,0].to_list()
+    Total_convertible_note_agreements_made_by_all_sharks = panda_df.iloc[:,1].to_list()
+    Total_revenue_sharing_agreement_made_by_all_sharks = panda_df.iloc[:, 2].to_list()
+    Total_royalty_agreement_made_by_all_sharks = panda_df.iloc[:, 3].to_list()
+    Total_loan_agreement_made_by_all_sharks = panda_df.iloc[:, 4].to_list()
+
+
+    venn2([set(Barbara_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+          set_colors=('#3E64AF', '#3EAF5D'),
+          set_labels=('Total\ninvestments\n of Barbara',
+                      'Total\n Equity investment \n  agreement'),
+          alpha=0.75)
+    venn2_circles([set(Barbara_total_investment),
+                   set(Total_equity_investments_agreements_made_by_all_sharks)], lw=0.7)
+    plt.show()
+
+########################################################################################################################
+
+    labels = ['Barbara',
+              'Mark',
+              'Lori',
+              'Robert',
+              'Daymond',
+              'Kevin']
+
+    labels_agreements = ["Equity investment agreement",
+                         "Convertible note agreement",
+                         "Revenue-sharing agreement",
+                         "Royalty agreement",
+                         "Loan agreement"]
+
+    c = ('#3E64AF', '#3EAF5D')
+
+    # subplot indexes
+    txt_indexes = [1, 7, 13, 19, 25]
+    title_indexes = [2, 9, 16, 23, 30]
+    plot_indexes = [8, 14, 20, 26, 15, 21, 27, 22, 28, 29]
+
+
+    # combinations of sets
+    title_sets = [[set(Barbara_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+                  [set(Barbara_total_investment), set(Total_convertible_note_agreements_made_by_all_sharks)],
+                  [set(Barbara_total_investment), set(Total_revenue_sharing_agreement_made_by_all_sharks)],
+                  [set(Barbara_total_investment), set(Total_royalty_agreement_made_by_all_sharks)],
+                  [set(Barbara_total_investment), set(Total_loan_agreement_made_by_all_sharks)]]
+
+    plot_sets = [[set(Barbara_total_investment), set(Total_royalty_agreement_made_by_all_sharks)],
+                 [set(Barbara_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+                 [set(Barbara_total_investment), set(Total_revenue_sharing_agreement_made_by_all_sharks)],
+                 [set(Barbara_total_investment), set(Total_convertible_note_agreements_made_by_all_sharks)],
+                 [set(Mark_total_investment), set(Total_royalty_agreement_made_by_all_sharks)],
+                 [set(Mark_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+                 [set(Mark_total_investment), set(Total_convertible_note_agreements_made_by_all_sharks)],
+                 [set(Lori_total_investment), set(Total_royalty_agreement_made_by_all_sharks)],
+                 [set(Lori_total_investment), set(Total_equity_investments_agreements_made_by_all_sharks)],
+                 [set(Daymond_total_investment), set(Total_royalty_agreement_made_by_all_sharks)]]
+
+
+
+
+    fig, ax = plt.subplots(1, figsize=(16,16))
+    # plot texts
+    for idx, txt_idx in enumerate(txt_indexes):
+        plt.subplot(6, 6, txt_idx)
+        plt.text(0.5,0.5,
+                 labels[idx+1],
+                 ha='center', va='center', color='#1F764B')
+        plt.axis('off')
+    # plot top plots (the ones with a title)
+    for idx, title_idx in enumerate(title_indexes):
+        plt.subplot(6, 6, title_idx)
+        venn2(title_sets[idx], set_colors=c, set_labels = (' ', ' '))
+        plt.title(labels[idx], fontsize=10, color='#1F4576')
+
+
+        # plot the rest of the diagrams
+    for idx, plot_idx in enumerate(plot_indexes):
+        plt.subplot(6, 6, plot_idx)
+        venn2(plot_sets[idx], set_colors=c, set_labels = (' ', ' '))
+
+
+    plt.show()
