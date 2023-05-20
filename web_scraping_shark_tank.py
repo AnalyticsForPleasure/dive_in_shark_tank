dd4a9b44bbefdd2b4108f6523a07f28cfffc1418
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-20 09:43:32 +0300

    Fixing scatter plot

--- web_scraping_shark_tank.py
+++ web_scraping_shark_tank.py
@@ -92,7 +92,7 @@ def scaling_the_scraped_data_in_order_to_get_to_the_top_n_viewers_episode_in_ase
     # Adding another column - "Percent" :
     top_eight_episode['Percent'] = [round(i * 100 / sum(top_eight_episode.viewers), 1) for i in
                                     top_eight_episode.viewers]
-    top_eight_episode['Percent'] = (top_eight_episode['Percent']).apply(lambda x: "{0:.2f}".format(x))+'%'
+    top_eight_episode['Percent'] = (top_eight_episode['Percent']).apply(lambda x: "{0:.2f}".format(x)) + '%'
     # Adding "M" for the viewers:
     top_eight_episode['viewers'] = top_eight_episode['viewers'].astype(float)
     top_eight_episode['viewers'] = (top_eight_episode['viewers']).apply(lambda x: "{0:.2f}".format(x))  # +'M'
@@ -101,13 +101,13 @@ def scaling_the_scraped_data_in_order_to_get_to_the_top_n_viewers_episode_in_ase
     list_x = list(range(0, len(top_eight_episode)))
     top_eight_episode['X Position'] = list_x
 
-    #top_eight_episode['episode'] = top_eight_episode['episode'].astype(str)
-
+    top_eight_episode['episode'] = top_eight_episode['episode'].astype(str)
 
     top_eight_episode.dtypes
     print('*')
 
     return top_eight_episode
+
 
 # ******************************************************************************************************************
 # Function  name: Part 3 -- "visualizing_the_number_of_viewers_for_each_season_with_bubble_chart"
@@ -116,28 +116,33 @@ def scaling_the_scraped_data_in_order_to_get_to_the_top_n_viewers_episode_in_ase
 # ******************************************************************************************************************
 def visualizing_the_number_of_viewers_for_each_season_with_bubble_chart(top_eight_episode):
     pal_ = list(sns.color_palette(palette='crest', n_colors=len(top_eight_episode)).as_hex())
-    #cmap = sns.color_palette("Blues", as_cmap=True).as_hex()
+    # cmap = sns.color_palette("Blues", as_cmap=True).as_hex()
     # Now' let's go to the charting part:
     # create a labels list for each bubble
-    label = [f'Episode{i}'+'<br>'+ str(j) +'<br>'+ str(k) for i, j, k in zip(top_eight_episode['episode'],
-                                                                                top_eight_episode['viewers'],
-                                                                                top_eight_episode['Percent'])]
+    label = [f'Episode {i} <br> {j} Million <br> {k}' for i, j, k in
+             zip(top_eight_episode['episode'],
+                 top_eight_episode['viewers'],
+                 top_eight_episode['Percent'])]
 
-    # label = [f'Episode{i}'+'<br>'+ str(j) +'Millions'+'<br>'+ str(k) for i, j, k in zip(top_eight_episode['episode'],
-    #                                                                                     top_eight_episode['viewers'],
-    #                                                                                     top_eight_episode['Percent'])]
+    # Before passing the array of viewers into into the scatter plot we should
+    # cast into float since it was string:
+    # [3.56 3.96 4.23 4.3  4.4  4.44 4.65 4.79]
+    # but the numbers are very close to each other, so we would like to rescale it using Exponential function:
+    top_eight_episode['viewers'] = 50 ** top_eight_episode['viewers'].astype(float)
 
-
+    # Now we got a better seperation between the numbers so we will be able to see the difference sizes much more
+    # clearly, output:
+    # [1117720.497, 5344673.95, 15368911.80 , 20210218.95, 29886015.61, 34948361.49, 79471337.11, 137425181.99)
 
     fig = px.scatter(top_eight_episode,
-                         x=list(top_eight_episode.loc[:, 'X Position']),
-                         y=list(top_eight_episode.loc[:, 'Y Position']),
-                         color=top_eight_episode['episode'],
-                         color_discrete_sequence=pal_,
-                         size=top_eight_episode['viewers'],
-                         text=label,
-                         size_max=15)
-    fig.update_layout(width=900, height=320,margin=dict(t=50, l=0, r=0, b=0),showlegend=False)
+                     x=list(top_eight_episode.loc[:, 'X Position']),
+                     y=list(top_eight_episode.loc[:, 'Y Position']),
+                     color=top_eight_episode['episode'],
+                     color_discrete_sequence=pal_,
+                     size=top_eight_episode['viewers'],
+                     text=label,
+                     size_max=50)
+    fig.update_layout(width=900, height=320, margin=dict(t=50, l=0, r=0, b=0), showlegend=False)
     fig.update_traces(textposition='top center')
     fig.update_xaxes(showgrid=False, zeroline=False, visible=False)
     fig.update_yaxes(showgrid=False, zeroline=False, visible=False)
@@ -147,24 +152,17 @@ def visualizing_the_number_of_viewers_for_each_season_with_bubble_chart(top_eigh
     print('*')
 
 
-
-
 if __name__ == '__main__':
-
-
-
     # df = pd.read_excel(r'C:/Users/Gil/PycharmProjects/dive_into_shark_tank/Data/shark_tank_data.xlsx',
     #                    sheet_name='Sheet1')
     ########################################################################################################################
     # path 1 : retireiving the data from wiki web page:
     url = 'https://en.wikipedia.org/w/index.php?title=List_of_Shark_Tank_episodes&oldid=911241643'
 
-    res=scraping_the_wiki_web_page_of_sark_tank_viewers(url)
+    res = scraping_the_wiki_web_page_of_sark_tank_viewers(url)
     ########################################################################################################################
     # #Path 2 : Scaling the data of top 10 episodes viewer of each year
 
-    res_2 =scaling_the_scraped_data_in_order_to_get_to_the_top_n_viewers_episode_in_aseason(res)
+    res_2 = scaling_the_scraped_data_in_order_to_get_to_the_top_n_viewers_episode_in_aseason(res)
     visualizing_the_number_of_viewers_for_each_season_with_bubble_chart(res_2)
     print('*')
-#trying to dubug untill line 130. chan
-
