58c501a76d2572c41825d78585fbb2f2c4e5dc67
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-23 18:56:48 +0300

    creating a table which retrieve us the amount of enterpreneurs in each category [mixed teams, female, male] for every shark

--- horizontal_chart_animation.py
+++ horizontal_chart_animation.py
@@ -17,8 +17,33 @@ if __name__ == '__main__':
     print('*')
 
 
-    closed_deals_first_season = df.loc[df['Deal'] == 'Yes',:]
+    all_the_deals_closed = df.loc[df['Deal'] == 'Yes',:]
+    all_the_deals_closed = all_the_deals_closed.replace(np.nan, '', regex=True)
+
+
+    gender_table = pd.DataFrame({'Mixed Team': [],
+                                'Female': [],
+                                'Male': []})
+
+    groups_by_gender = all_the_deals_closed.groupby('Entrepreneur Gender')
+    for gender, mini_df_gender in groups_by_gender:
+        mini_df_gender.reset_index(inplace=True, drop=True)
+        gender_table_matrix = mini_df_gender.loc[:, 'Barbara\nCorcoran':'Guest']
+        gender_table_matrix = gender_table_matrix.replace('', 0)
+        gender_table_matrix = gender_table_matrix.astype('int')
+        gender_table_matrix = gender_table_matrix.sum()
+        gender_table_matrix = gender_table_matrix.reset_index(level=0)
+        gender_table_matrix.rename(columns={gender_table_matrix.columns[0]: 'shark_name'}, inplace=True)
+        gender_table_matrix.rename(columns={gender_table_matrix.columns[1]: f'{gender}'}, inplace=True)
+
+        current_column = gender_table_matrix.loc[:,f'{gender}']
+        gender_table = pd.concat([gender_table, current_column], axis=1)
+
+
+        #df = pd.DataFrame({'col':L})
     print('*')
+
+
 
 
     # Sample data
