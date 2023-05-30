ca92ef913d59b2c8d42081c896af8471b3396fa3
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-05-30 20:01:24 +0300

    Finishing the area chart one layer + adding gradient color. need to add annotation

--- number_of_episodes_in_season.py
+++ number_of_episodes_in_season.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import dataframe_image as dfi
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap # in order to add the gradient color
 
 # **************************************************************************************************************
 # Function  name: get_number_of_investments_each_season_over_the_years
@@ -29,25 +30,48 @@ def creating_gradient_area_bar_chart_for_the_investments_over_the_seasons(table)
     print('*')
     number_of_season = list(table.loc[:,'season_number'])
     number_of_investments_over_each_season = list(table.loc[:,'Amount_of_investments_over_each_season'])
-    plt.fill_between(number_of_season,
-                     number_of_investments_over_each_season,
-                     color="skyblue",
-                     alpha=0.4)
+
+    # Adding blue gradient color:
+    x=np.arange(1,11,1)
+    y1= np.array(number_of_investments_over_each_season)
+    y2= np.repeat(0, 10)
+    print('*')
+    cm1 = LinearSegmentedColormap.from_list('Temperature Map', ['navy','skyblue'])
+    polygon = plt.fill_between(x, y1, y2, lw=0, color='none')
+    xlim = (x.min(), x.max())
+    ylim = plt.ylim()
+    verts = np.vstack([p.vertices for p in polygon.get_paths()])
+    gradient = plt.imshow(np.linspace(0, 1, 256).reshape(-1, 1), cmap=cm1, aspect='auto', origin='lower',
+                          extent=[verts[:, 0].min(), verts[:, 0].max(), verts[:, 1].min(), verts[:, 1].max()])
+    gradient.set_clip_path(polygon.get_paths()[0], transform=plt.gca().transData)
+
     plt.plot(number_of_season,
              number_of_investments_over_each_season,
-             color="Slateblue",
+             color="Gray",
              alpha=0.6,
-             linewidth=2)
+             linewidth=3)
     plt.tick_params(labelsize=12)
-    #plt.xticks(np.arange(10)+1, np.arange(10)+1)
-    plt.xlabel('Season Number', size=17)
-    plt.ylabel('Number of Investments made over each season', size=17)
+    plt.xticks(number_of_season, number_of_season)
+
+    plt.xlabel('Season Number', fontsize=14,fontweight='bold',fontname='Franklin Gothic Medium Cond')
+    plt.ylabel('Number of Investments', fontsize=14,fontweight='bold',fontname='Franklin Gothic Medium Cond')
     plt.ylim(bottom=0)
+    plt.title('Number of investments made during each season' ,fontsize=22, weight='bold',fontname='Franklin Gothic Medium Cond')
+    print('*')
+    plt.xlim(xlim)
+    plt.ylim(ylim)
+
+# annotate
+    # for x, v in table.iterrows():
+    #     cs = v.cumsum()[::-1]  # get the cumulative sum of the row and reverse it to provide the correct y position
+    #     for i, a in enumerate(v[::-1]):  # reverse the row values for the correct annotation
+    #         plt.annotate(text=f'${a:0.2f}', xy=(x, cs[i]))
 
     # Displaying the chart
     plt.show()
 
 if __name__ == '__main__':
+
     pd.set_option('display.max_rows', 900)
 
     df = pd.read_excel(r'C:/Users/Gil/PycharmProjects/dive_into_shark_tank/Data/shark_tank_data.xlsx',sheet_name='Sheet1')  # Ten seasons
