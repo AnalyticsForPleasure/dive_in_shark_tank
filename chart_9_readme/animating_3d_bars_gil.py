eb8471d1db3b7000e7acdab46493b7968cececa6
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-06-17 16:23:01 +0300

    stiching the gifs - TODO: find out why thr last  row isn't working

--- chart_9_readme/animating_3d_bars_gil.py
+++ chart_9_readme/animating_3d_bars_gil.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import pandas as pd
+import imageio
 
 
 def animate(i, input_heights):
@@ -55,7 +56,7 @@ if __name__ == '__main__':
     colors = np.array(['skyblue', 'dodgerblue', 'lightseagreen','slategray','navy','lightsteelblue'] * len(industries_names)).reshape((4, 6))
     # dx, dy, dz : float or array-like
     # The width, depth, and height of the bars, respectively:
-    bar_width = 0.2
+    bar_width = 0.1
     bar_depth = 0.2
 
     width_vec = [bar_width] * xx_table.shape[1]
@@ -74,4 +75,24 @@ if __name__ == '__main__':
                              blit=True)
         anim.save(f'images/anim_{idx}.gif', writer='imagemagick')
 
-        # matplotlib.animation.FileMovieWriter(*args, **kwargs)[source] TODO: "FileMovieWriter" - Ask gil if to add this line?
+########################################################################################################################
+    #stiching the gifs
+    # List of GIF files to stitch together
+    gif_files = ["C:/Users/Gil/PycharmProjects/dive_into_shark_tank/chart_9_readme/images/anim_0.gif",
+                 "C:/Users/Gil/PycharmProjects/dive_into_shark_tank/chart_9_readme/images/anim_1.gif",
+                 "C:/Users/Gil/PycharmProjects/dive_into_shark_tank/chart_9_readme/images/anim_2.gif",
+                 "C:/Users/Gil/PycharmProjects/dive_into_shark_tank/chart_9_readme/images/anim_3.gif"]
+
+    # Create a list to hold the frames of the stitched GIF
+    frames = []
+
+    # Iterate over the GIF files
+    for gif_file in gif_files:
+        # Read the frames from the current GIF file
+        gif_frames = imageio.mimread(gif_file)
+        # Extend the frames list with the frames from the current GIF file
+        frames.extend(gif_frames)
+
+    # Save the stitched GIF file
+    output_file = "C:/Users/Gil/PycharmProjects/dive_into_shark_tank/chart_9_readme/images/final_animation/stitched"
+    imageio.mimsave(output_file, frames, format='GIF', duration=0.1)  # stiching the gifs - ODO: find out why thr last  row isn't working
